1ac4cd8d1f2b9e730ad3151d4be5a5555316a896
seungjun-green <lsj3285007@gmail.com>
2023-11-30 00:13:23 +0900

    Initial Commit

---
+++ Photo Talk/Photo_TalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_TalkApp.swift
+//  Photo Talk
+//
+//  Created by SeungJun Lee on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Photo_TalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
